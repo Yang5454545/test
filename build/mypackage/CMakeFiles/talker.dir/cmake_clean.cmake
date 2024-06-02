b46d561ede2c685774455e194fe3903d2c0c0bc0
ubuntu18 <3326336752@qq.com>
2024-06-02 02:43:35 -0700

    first commit

---
+++ build/mypackage/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+  "/home/xj/Desktop/catkin_ws/devel/lib/mypackage/talker.pdb"
+  "/home/xj/Desktop/catkin_ws/devel/lib/mypackage/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/mypackage/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xj/Desktop/catkin_ws/src/mypackage
+# Build directory: /home/xj/Desktop/catkin_ws/build/mypackage
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
